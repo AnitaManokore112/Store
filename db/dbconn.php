8b8cd2b20270c07d526084b4ce1f9a35e984f967
AnitaManokore112 <119316437+AnitaManokore112@users.noreply.github.com>
2022-12-05 17:10:11 +0200

    Update dbconn.php

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,4 +1,4 @@
 <?php
-	$conn=mysqli_connect('https://www.dp4free.net','anitamanokore112','S78_NefP!9Fj7FN','anitaware') or die(mysql_error());
+	$conn=mysqli_connect('https://www.dp4free.net','anitamanokore112','S78_NefP!9Fj7FN','alphaware') or die(mysql_error());
 ?>
 
